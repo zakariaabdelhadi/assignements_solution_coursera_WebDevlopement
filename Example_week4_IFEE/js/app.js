6886a633791f091ff648ae46f89ea21e8c1bf674
zakaria <zakizermani@gmail.com>
2021-02-27 09:32:42 +0100

    to submit

---
+++ Example_week4_IFEE/js/app.js
@@ -0,0 +1,10 @@
+yaakovGreeter.sayHello();
+johnGreeter.sayHi();
+
+console.log(yaakovGreeter);
+//
+// // Immediately Invoked Function Expression
+// // IIFE
+// (function (name) {
+//   console.log("Hello " + name);
+// })("Coursera!");
